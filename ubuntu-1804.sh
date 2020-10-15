27e37b3919f1e09acad151f972141497bbada803
Muhammad Ilham <muhd.iiilham@gmail.com>
2020-10-15 08:33:25 +0700

    done

--- ubuntu-1804.sh
+++ ubuntu-1804.sh
@@ -2,40 +2,31 @@ echo "Installing Docker"
 
 # Ofc, Always Update
 # Before Anything Else
+echo "=== Running apt update & upgrad ==="
 sudo apt update -y
 sudo apt upgrade -y
 
 # Package Needed
+echo "=== Adding Necessary Package ==="
 sudo apt-get install curl apt-transport-https ca-certificates software-properties-common -y
 
 # Add Docker Repo
+echo "=== Adding Docker Repository ==="
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update -y
 
 # Installing Docker-ce
+echo "=== Installing docker-ce ==="
 sudo apt install docker-ce -y
 
 # Adding TO Docker Group
+echo "=== Adding Current User To Docker Group ==="
 sudo usermod -aG docker $USER
 newgrp docker
 
-# Test If Docker Could Running
-# Withour `sudo`
-docker run hello-world
-
-echo "Installing Docker-Compose"
-
-# Download Current Stable
-# Release Of Docker-Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-# Apply executable permissions to the binary
-sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-# Test Installation
-docker-compose --version
+# Install Docker Compose With apt
+echo "=== Installing docker-compose with apt ==="
+sudo apt install docker-compose
 
 echo "Script Is Done, Have A Nice Day!"
-
